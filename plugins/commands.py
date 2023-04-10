a5bf6a818e41a623b43c86d022ad59e1ac2a50e4
Ruban Durai <77948100+RubanDurai@users.noreply.github.com>
2023-04-10 21:28:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,6 +18,10 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
+@Client.on_message(filters.service)
+async def main(client , message):
+    await message.delete()
+
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
